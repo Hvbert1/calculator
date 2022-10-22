76183a3950bcb75bc4239250d359a739ff7ff46d
Hvbert1 <henryjiafu@gmail.com>
2022-10-22 20:30:16 +1100

    add class calculator and add query selectors on all buttons

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-var result = "";
+var a = null;
+var b = "";
+var valueb = "";
+var result ="";
+const numberButtons = document.querySelectorAll('#digits');
+const operationsButtons = document.querySelectorAll('#btns');
+const equalsButton = document.querySelector('.equals');
+const divideButton = document.querySelector('.equals');
+const timesButton = document.querySelector('.equals');
+const equalsButton = document.querySelector('.equals');
+const previousOperand = document.querySelector('.previous-operand');
+const currentOperand = document.querySelector('.current-operand');
 
+
+class Calculator {
+    constructor(previousOperand, currentOperand) {
+        this.previousOperand = previousOperand;
+        this.currentOperand  = currentOperand;
+    }
+}
 function display(value) {
     document.getElementById("result").value += value;
+    valueb += value;
 }
 
-function add(a, b) {
-    result = a + b;
+function store() {
+    if(a == null) {
+        a = valueb.slice(0, -1);
+        console.log("if a: " + a);
+        valueb = "";
+    }
+    else {
+        b = valueb.slice(0, -1);
+        console.log("if b: " + b);
+        valueb = "";
+    }
+}
+
+function add() {
+    result = parseInt(a) + parseInt(b);
 }
 
 function subtract(a, b) {
@@ -20,7 +52,8 @@ function divide(a, b) {
     result = a / b;
 }
 
-function operate(a, b) {
-    add(a, b);
+function operate() {
+    add();
     console.log(result);
+    a = result;
 }
