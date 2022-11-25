c4524069d53cfac7096152f38fe7f31fa7d9b4d7
Hvbert1 <henryjiafu@gmail.com>
2022-11-26 10:17:35 +1100

    change variable names

--- script.js
+++ script.js
@@ -1,88 +1,88 @@
-var first = null;
-var second = "";
-var current = "";
-var result ="";
+var a = null;
+var b;
+var curr = ""; //current value
+var result = "";
 var displayRes="";
-var operation = "";
-
-function operatePlus() {
-    operation = "plus";
-}
-function operateMinus() {
-    operation = "minus";
-}
-function operateDivide() {
-    operation = "divide";
-}
-function operateTimes() {
-    operation = "times";
-}
+var prevOperation = null;
+var nextOperation;
 
 function display(value) {
     document.getElementById("result").value += value;
-    current += value;
+    curr += value;
 }
 
 function displayResult() {
-    document.getElementById("result2").value = displayRes;
+    if(!isNaN(result)) {
+        document.getElementById("result2").value = displayRes;
+    }
 }
 
 function store() {
-    if(first == null) {
-        first = current.slice(0, -1);
-        console.log("if a: " + first);
-        current = "";
+    if(a == null) {
+        a = curr;
+        curr = "";
     }
     else {
-        second = current.slice(0, -1);
-        console.log("if b: " + second);
+        b = curr;
     }
 }
 
+// function storeOperation(event) {
+//     if(operation == null) {
+//         current_operation = event.target.id;
+//         console.log("first Op: " + operation);
+//     }
+//     else {
+//         operation2 = event.target.id;
+//         console.log("second Op: " + operation2);
+//     }
+// }
+
 function operate() {
-    if(operation == "plus") {
-        add();
-    }
-    else if (operation == "divide") {
-        divide();
-    }
-    else if (operation == "minus") {
-        subtract();
-    }
-    else if (operation == "times") {
-        multiply();
+    if(prevOperation != undefined) {
+        if(prevOperation == "plus") {
+            add();
+        }
+        else if (prevOperation == "over") {
+            divide();
+        }
+        else if (prevOperation == "minus") {
+            subtract();
+        }
+        else if (prevOperation == "times") {
+            multiply();
+        }
     }
     console.log(result);
-    display(result);
     displayRes = result;
-    clear();
 }
 
 function clear() {
-    first = null;
-    second = "";
-    current = "";
-    result ="";
+    a = null;
+    prevOperation = null;
+    b = "";
+    curr = "";
+    result = "";
+    operation2 = "";
     document.getElementById("result").value = "";
-    console.log('cleared');
 }
 
 function add() {
-    result = parseFloat(first) + parseFloat(second);
+    result = parseFloat(a) + parseFloat(b);
 }
 
 function subtract() {
-    result = parseFloat(first) - parseFloat(second);
+    result = parseFloat(a) - parseFloat(b);
 }
 
 function multiply() {
-    result = parseFloat(first) * parseFloat(second);
+    result = parseFloat(a) * parseFloat(b);
 }
 
 function divide() {
-    result = parseFloat(first) / parseFloat(second);
-    if(parseInt(first) % parseInt(second) == 0) {
-        result = parseInt(first) / parseInt(second);
+    result = parseFloat(a) / parseFloat(b);
+    if(parseInt(a) % parseInt(b) == 0) {
+        result = parseInt(a) / parseInt(b);
     } else {
         result = result.toFixed(3);
     }
@@ -92,3 +92,34 @@ function reset() {
     clear();
     document.getElementById("result2").value = "";
 }
+
+// const btnDigits = document.querySelectorAll('.digits')
+
+// btnDigits.forEach(btn => {
+//     btn.addEventListener('click', event => {
+//         console.log(event.target.value);
+//     });
+//     check();
+// });
+
+function clearCurrent() {
+    curr = ""; //current value
+    document.getElementById("result").value = "";
+}
+
+const btnOperators = document.querySelectorAll('.btns')
+
+btnOperators.forEach(btn => {
+    btn.addEventListener('click', event => {
+        if(prevOperation == null) {
+            current_operation = event.target.id;
+        }
+        else {
+            operation2 = event.target.id;
+        }
+        store();
+        operate();
+        clearCurrent();
+        displayResult();
+    });
+});
